6dc48abac7aa061002885521a588f6035e272c1c
Alex Ewing <awewing@pop-os.localdomain>
2025-05-22 22:21:59 -0700

    Save and quit keybinds. Enhance paste.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -4,15 +4,15 @@ lsp_zero.on_attach(function(client, bufnr)
     lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+-- why doesn't this work...
+--vim.keymap.set("n", "<leader>kl", vim.lsp.buf.code_action({ only={"quickfix"}, }))
+
 -- here you can setup the language servers
 require('mason').setup({})
 require('mason-lspconfig').setup({
   -- Replace the language servers listed here
   -- with the ones you want to install
   ensure_installed = {
-      'tsserver',
-      'eslint',
-      'rust_analyzer'
   },
   handlers = {
     function(server_name)
--- lua/awewing/remap.lua
+++ lua/awewing/remap.lua
@@ -9,16 +9,23 @@ vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
 -- Allow visual paste no-replace
-vim.keymap.set("x", "<leader>p", "\"_dP")
+vim.keymap.set("x", "p", "\"_dP")
 
--- Allow yank to system clipboard
+-- Allow yank/paste to system clipboard
+vim.keymap.set("n", "<leader>p", "\"+p")
+vim.keymap.set("v", "<leader>p", "\"+p")
+vim.keymap.set("n", "<leader>P", "\"+P")
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
 vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 -- Closing brackets
-vim.keymap.set("i", "{", "{}<Left>")
-vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>O")
+-- vim.keymap.set("i", "{", "{}<Left>")
+-- vim.keymap.set("i", "{<CR>", "{<CR>}<Esc>O")
 
 -- leader
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Save and quit
+vim.keymap.set("n", "<leader>s", ":update<cr>")
+vim.keymap.set("n", "<leader>q", "ZQ")
