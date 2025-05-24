1a952482fc61ba3afefa4e8907b42388f78f8feb
Alex Ewing <awewing@pop-os.localdomain>
2025-05-24 15:00:46 -0700

    Telescope keybinds and multigrep WIP

--- lua/config/plugins/telescope.lua
+++ lua/config/plugins/telescope.lua
@@ -11,13 +11,31 @@ return {
       defaults = {
         preview = {
           treesitter = {
-            -- Disable treesitter for json. Fixes bug that took me hours, holy.
-            disable = { "json" }
+            -- Only enable treesitter for languages we know we have an lsp installed for
+            --  ...I think. I don't understand this 100%
+            enable = { "lua", "go" }
           }
         }
       },
     }
 
-    vim.keymap.set("n", "<C-p>", require('telescope.builtin').find_files)
+    require("telescope").load_extension('fzf')
+
+    local builtin = require('telescope.builtin')
+    vim.keymap.set("n", "<C-p>", builtin.find_files)
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags)
+    -- Edit neovim
+    vim.keymap.set("n", "<leader>en", function()
+      builtin.find_files {
+        cwd = vim.fn.stdpath("config")
+      }
+    end)
+    vim.keymap.set("n", "<leader>ep", function()
+      builtin.find_files {
+        cwd = vim.fs.joinpath(vim.fn.stdpath("data"), "lazy")
+      }
+    end)
+
+    require("config.telescope.multigrep").setup()
   end,
 }
