d1ac2b6615638ed639269e9554cd44d86b9e4095
Alex Ewing <uy1977@exactsciences.com>
2025-05-28 10:40:58 -0700

    Fix typo in treesitter config

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "vim-treesitter/nvim-treesitter",
+    "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
       require 'nvim-treesitter.configs'.setup {
