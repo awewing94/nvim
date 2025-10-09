0dd2ff5132b69985abab44b58faf310a2dbe0a8f
Alex Ewing <awewing94@gmail.com>
2025-10-09 08:38:26 -0700

    Enable plugins. Add oil and harpoon. Harpoon WIP

--- init.lua
+++ init.lua
@@ -769,7 +769,11 @@ require("lazy").setup({
 				-- Disable "format_on_save lsp_fallback" for languages that don't
 				-- have a well standardized coding style. You can add additional
 				-- languages here or re-enable it for the disabled ones.
-				local disable_filetypes = { c = true, cpp = true }
+				local disable_filetypes = {
+					c = true,
+					cpp = true,
+					lua = true,
+				}
 				if disable_filetypes[vim.bo[bufnr].filetype] then
 					return nil
 				else
@@ -1003,18 +1007,18 @@ require("lazy").setup({
 	--  Here are some example plugins that I've included in the Kickstart repository.
 	--  Uncomment any of the lines below to enable them (you will need to restart nvim).
 	--
-	-- require 'kickstart.plugins.debug',
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
-	-- require 'kickstart.plugins.autopairs',
-	-- require 'kickstart.plugins.neo-tree',
-	-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+	require("kickstart.plugins.debug"),
+	require("kickstart.plugins.indent_line"),
+	-- require("kickstart.plugins.lint"),
+	require("kickstart.plugins.autopairs"),
+	-- require("kickstart.plugins.neo-tree"),
+	require("kickstart.plugins.gitsigns"), -- adds gitsigns recommend keymaps
 
 	-- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
 	--    This is the easiest way to modularize your config.
 	--
 	--  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-	-- { import = 'custom.plugins' },
+	{ import = "custom.plugins" },
 	--
 	-- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
 	-- Or use telescope!
